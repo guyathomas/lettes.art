6cd7f286002313c0300dc92000a64931e2fc858f
Guy Thomas <guy.thomas@instacart.com>
2021-06-11 12:07:40 -0700

    clean up about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,32 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
-import styled from '@emotion/styled';
+import { Box, Typography, Grid } from "@material-ui/core";
+import styled from "@emotion/styled";
 
 const AboutImage = styled.img`
-    width: 100%;
-    margin-top: 1rem;
-`
+  width: 100%;
+`;
 const About: React.FC = () => (
   <Box>
     <Typography variant="h5">About me</Typography>
-    <Typography variant="body1">
-      I'm a artist based in Mt. Martha, Victoria, Australia. The mediums I love
-      are acrylic, graphite and watercolour. I love creating portraits, animals
-      and abstract art.
-    </Typography>
-    <AboutImage
-      
-      src={`https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/132825345_10224144733789170_4843583989557150274_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=19026a&_nc_ohc=VjKsWLDNJO4AX8m7lX0&_nc_ht=scontent-sjc3-1.xx&oh=bb406ebf8fc6079f2d020754bee79ad2&oe=60C7E7E6`}
-    />
+    <Grid container spacing={2} mt={2}>
+      <Grid item md={6} xs={12}>
+        <Typography variant="body1">
+          I'm a self taught artist based on the Mornington Peninsula, Melbourne
+          Victoria. The mediums I like to use are Graphite, Watercolour and
+          Acrylic.
+        </Typography>
+        <Typography marginTop={2} variant="body1">
+          My work has been displayed in cafe's, stores and local markets. I also
+          create customised pieces for clients and more than happy to discuss
+          creating the perfect piece for you.
+        </Typography>
+      </Grid>
+      <Grid item md={6} xs={12}>
+        <AboutImage
+          src={`https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/132825345_10224144733789170_4843583989557150274_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=19026a&_nc_ohc=VjKsWLDNJO4AX8m7lX0&_nc_ht=scontent-sjc3-1.xx&oh=bb406ebf8fc6079f2d020754bee79ad2&oe=60C7E7E6`}
+        />
+      </Grid>
+    </Grid>
   </Box>
 );
 export default About;
