e26c2d41f8c0c5def0e6c204950c93cb6a7ff271
Guy Thomas <guythomas721@gmail.com>
2022-07-01 09:00:25 -0700

    upgrade mui

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,27 +1,25 @@
 import React from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { createTheme, adaptV4Theme } from "@mui/material";
-import { ThemeProvider as MUIThemeProvider, Theme, StyledEngineProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material";
+import { ThemeProvider as MUIThemeProvider, Theme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
-
-declare module '@mui/styles/defaultTheme' {
+declare module "@mui/styles/defaultTheme" {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface DefaultTheme extends Theme {}
 }
-
 
 const ThemeProvider: React.FC = ({ children }) => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
   const theme = React.useMemo(
     () =>
-      createTheme(adaptV4Theme({
+      createTheme({
         palette: {
           mode: "dark",
           // mode: prefersDarkMode ? "dark" : "light",
         },
-      })),
+      }),
     [prefersDarkMode]
   );
 
