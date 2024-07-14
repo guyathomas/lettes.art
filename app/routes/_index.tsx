246695815b858bf59c63f03de8112f1962389247
Guy Thomas <guythomas721@gmail.com>
2024-07-14 17:27:49 -0400

    fix filtering almost

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { Form, useSearchParams , useLoaderData } from "@remix-run/react";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import React from 'react';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
+import { Form, useSearchParams, useLoaderData } from '@remix-run/react';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   Dialog,
   DialogContent,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
-import { Toggle } from "@/components/ui/toggle";
+} from '@/components/ui/dialog';
+import { Toggle } from '@/components/ui/toggle';
 
-import { contentfulClient } from "../models/contentful/controller";
+import { contentfulClient } from '../models/contentful/controller';
 
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
     { title: `Lette's Art` },
     {
-      name: "description",
-      content: "Artwork from the Mornington Peninsula, Australia",
+      name: 'description',
+      content: 'Artwork from the Mornington Peninsula, Australia',
     },
-    { name: "og:title", content: "Lette's Art" },
+    { name: 'og:title', content: "Lette's Art" },
     {
-      name: "og:image",
+      name: 'og:image',
       content:
-        "https://images.ctfassets.net/2y9b3o528x1d/6w7gZ9Y8k7h5ZJHf3zgZ3e/1d1f5e1e6",
+        'https://images.ctfassets.net/2y9b3o528x1d/6w7gZ9Y8k7h5ZJHf3zgZ3e/1d1f5e1e6',
     },
   ];
 };
 
-const MAX_IMAGE_WIDTH = "1179";
+const MAX_IMAGE_WIDTH = '1179';
 
 export async function loader() {
   const entries = await contentfulClient.getArtCollection();
@@ -43,7 +43,10 @@ export async function loader() {
 }
 
 enum QUERY_PARAMS {
-  ACTIVE_IMAGE_ID = "activeImageId",
+  ACTIVE_IMAGE_ID = 'activeImageId',
+  MEDIUM = 'medium',
+  SOLD_STATUS = 'soldStatus',
+  CATEGORY = 'category',
 }
 
 const Index: React.FC = () => {
@@ -55,33 +58,27 @@ const Index: React.FC = () => {
   if (!artwork?.length) {
     return <h2 className="text-primary">No Images Found 😢</h2>;
   }
-  const filteredArtwork = [...artwork];
-  // const filteredArtwork = React.useMemo(
-  //   () =>
-  //     artwork
-  //       .filter((art) => {
-  //         if (activeFilters.mediumPaint === null) return true;
-  //         return art.fields.mediumPaint.includes(activeFilters.mediumPaint);
-  //       })
-  //       .filter((art) => {
-  //         if (activeFilters.forSale === null) return true;
-  //         const forSale = (art.fields.forSale?.toString() ||
-  //           "false") as BooleanString;
-  //         const artMatchesActiveFilter =
-  //           activeFilters.forSale.includes(forSale);
-  //         return artMatchesActiveFilter;
-  //       })
-  //       .filter((art) => {
-  //         if (
-  //           activeFilters.category === null ||
-  //           activeFilters.category === "all"
-  //         ) {
-  //           return true;
-  //         }
-  //         return art.fields.category === activeFilters.category;
-  //       }),
-  //   [artwork, activeFilters]
-  // );
+  console.log(searchParams.get(QUERY_PARAMS.MEDIUM));
+  const filteredArtwork = React.useMemo(
+    () =>
+      artwork
+        .filter((art) => {
+          const medium = searchParams.get(QUERY_PARAMS.MEDIUM);
+          if (!medium) return true;
+          return art?.mediumPaint?.includes(medium);
+        })
+        .filter((art) => {
+          const soldStatus = searchParams.get(QUERY_PARAMS.SOLD_STATUS);
+          if (!soldStatus) return true;
+          return soldStatus === art?.forSale?.toString();
+        })
+        .filter((art) => {
+          const cateogry = searchParams.get(QUERY_PARAMS.CATEGORY);
+          if (!cateogry) return true;
+          return art?.category === cateogry;
+        }),
+    [artwork, searchParams],
+  );
   const activeArtwork = artwork.find((art) => art?._id === activeImageId);
   const modalUrl = activeArtwork?.imagesCollection?.items[0]?.url;
   return (
@@ -94,7 +91,7 @@ const Index: React.FC = () => {
             </h2>
           </DialogTitle>
           <img
-            src={modalUrl ? modalUrl + `?w=1800&fm=webp` : ""}
+            src={modalUrl ? modalUrl + `?w=1800&fm=webp` : ''}
             className="object-cover object-center max-h-[75vh] max-w-[85vw] md:max-w-[75vw]"
           />
         </DialogContent>
@@ -102,24 +99,27 @@ const Index: React.FC = () => {
           <h1 className="text-primary scroll-m-20 text-4xl tracking-tight lg:text-5xl uppercase my-4 sticky top-0 bg-background z-10 py-2">
             {`Lette's Art`}
           </h1>
-          <div className="flex mb-4 gap-x-8 gap-y-2 flex-wrap">
+          <Form className="flex mb-4 gap-x-8 gap-y-2 flex-wrap">
             <ToggleGroup type="single" variant="outline">
               <ToggleGroupItem
-                value="bold"
+                value="Acrylic"
+                name={QUERY_PARAMS.MEDIUM}
                 aria-label="Toggle Acrylic"
                 className="text-primary"
               >
                 Acrylic
               </ToggleGroupItem>
               <ToggleGroupItem
-                value="italic"
+                value="Graphite"
+                name={QUERY_PARAMS.MEDIUM}
                 aria-label="Toggle Graphite"
                 className="text-primary"
               >
                 Graphite
               </ToggleGroupItem>
               <ToggleGroupItem
-                value="strikethrough"
+                value="Watercolour"
+                name={QUERY_PARAMS.MEDIUM}
                 aria-label="Toggle Watercolour"
                 className="text-primary"
               >
@@ -128,14 +128,16 @@ const Index: React.FC = () => {
             </ToggleGroup>
             <ToggleGroup type="single" variant="outline">
               <ToggleGroupItem
-                value="bold"
+                value="For Sale"
+                name={QUERY_PARAMS.SOLD_STATUS}
                 aria-label="Toggle For Sale"
                 className="text-primary"
               >
                 For Sale
               </ToggleGroupItem>
               <ToggleGroupItem
-                value="italic"
+                value="Sold"
+                name={QUERY_PARAMS.SOLD_STATUS}
                 aria-label="Toggle Sold"
                 className="text-primary"
               >
@@ -144,18 +146,20 @@ const Index: React.FC = () => {
             </ToggleGroup>
             <Toggle
               variant="outline"
+              value="Pet Portrait"
+              name={QUERY_PARAMS.CATEGORY}
               aria-label="Toggle Pet Portrait"
               className="text-primary"
             >
               Pet Portrait
             </Toggle>
-          </div>
+          </Form>
           <Form>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
               {filteredArtwork.map((artwork) => {
                 if (!artwork?.imagesCollection?.items.length) {
                   console.warn(
-                    `Artwork: ${artwork?.title || "?"} has no images`,
+                    `Artwork: ${artwork?.title || '?'} has no images`,
                   );
                   return null;
                 }
@@ -176,9 +180,9 @@ const Index: React.FC = () => {
                           src={
                             file?.url
                               ? file.url + `?w=${MAX_IMAGE_WIDTH}&fm=webp`
-                              : ""
+                              : ''
                           }
-                          alt={artwork.title || "artwork image"}
+                          alt={artwork.title || 'artwork image'}
                           loading="lazy"
                           width={`${MAX_IMAGE_WIDTH}px`}
                           className="rounded-b-sm h-full object-cover"
